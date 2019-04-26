1996f3268cce85aebb3d1188e4000cc310cb2467
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-26 22:22:36 +0100

    Boating School Labs Complete

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,59 @@
 class Instructor
+  @@all = []
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def student
+    teachers_student = []
+    BoatingTest.all.each do |data|
+      if data.instructor == self
+        teachers_student << data
+      end
+    end
+    teachers_student
+  end
+
+  def pass_student(student, testname)
+    #take in student argument and test_name
+    #If there is a BoatingTest who name and
+    # student match the values passed in
+    # This method will update the test_status to passed
+    # If no matching test this method will create the test
+
+    BoatingTest.all.each do |test_data|
+      if test_data.test_name == testname &&
+        test_data.student.object_id == student.object_id
+
+        test_data.test_status = "passed"
+      else
+        BoatingTest.new(student, testname, "passed", self.name)
+      end
+    end
+    BoatingTest.all.last
+  end
+
+  def fail_student(student, testname)
+    #oposite of above
+
+    BoatingTest.all.each do |test_data|
+      if test_data.test_name == testname &&
+        test_data.student.object_id == student.object_id
+
+        test_data.test_status = "failed"
+      else
+        BoatingTest.new(student, testname, "failed", self.name)
+      end
+    end
+    BoatingTest.all.last
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,13 +1,40 @@
+require 'pry'
 require_relative '../config/environment.rb'
 
 def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+### Student Tests ###
+sandy_student = Student.new("Sandy")
+spongebob_student = Student.new("Spongebob")
+plancton_student = Student.new("Plancton")
+patrick_student = Student.new("Patrick")
+squidworth_student = Student.new("Squidworth")
+
+### Instructor Tests ###
+mrs_puff = Instructor.new("Mrs Puff")
+mr_krabs = Instructor.new("Mr Krabs")
+lauren = Instructor.new("Lauren")
+
+### Boatingtest Tests ###
+st1 = BoatingTest.new(sandy_student, "Sailing First Timers", "failed", mrs_puff)
+st2 = BoatingTest.new(sandy_student, "Don't Crash 101", "passed", mrs_puff)
+st3 = BoatingTest.new(sandy_student, "How to Sail 101", "failed", mrs_puff)
+sbt1 = BoatingTest.new(spongebob_student, "Don't Crash 101", "failed", mr_krabs)
+sbt2 = BoatingTest.new(spongebob_student, "Don't give up trying", "failed", mr_krabs)
+sbt3 = BoatingTest.new(spongebob_student, "Don't Crash 101", "failed", mr_krabs)
+pt1 = BoatingTest.new(plancton_student, "Power Steering", "failed", lauren)
+pt2 = BoatingTest.new(plancton_student, "Don't Crash 101", "failed", lauren)
+pt3 = BoatingTest.new(plancton_student, "Don't give up trying", "passed", lauren)
+st1 = BoatingTest.new(squidworth_student, "Don't Crash 101", "passed", mr_krabs)
+pat1 = BoatingTest.new(patrick_student, "Advanced Sailing", "passed", mrs_puff)
+
+
+# student.add_boating_test(self, test_name, test_status, instructor)
+#### BoatingTest.new(self, test_name, test_status, instructor)
+
+#
 
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
-
+puts "Mischief Managed!"
